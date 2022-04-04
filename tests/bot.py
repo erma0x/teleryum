5a8683ebf5c306fc15a53939fcd58263fd53387e
erma0x <ermano.buikis@gmail.com>
2022-04-04 20:36:48 +0200

    reformat test

--- tests/bot.py
+++ tests/bot.py
@@ -17,11 +17,12 @@ load_dotenv()
 #                 'enableRateLimit': True,
 #                     })
 
+
                     
 if __name__ == "__main__":
 
     now = datetime.now()
-    print("START:", colored(now.strftime("%d/%m/%Y %H:%M:%S"),'yellow'))	
+    print("START:", colored(now.strftime("%d/%m/%Y %H:%M:%S"),'blue'))	
 
     load_dotenv()
     USERNAME_TELEGRAM = os.getenv('USERNAME_TELEGRAM')
@@ -31,9 +32,9 @@ if __name__ == "__main__":
     # connect to telegram client with my.telegram.com API
     client = TelegramClient(USERNAME_TELEGRAM, API_ID_TELEGRAM, API_HASH_TELEGRAM) 
     client.start()
-    print('trading-bot',colored('[ACTIVE]','green')+"\nTelegram client connected with: ",colored(USERNAME_TELEGRAM,'yellow'))
-
-# freecrypto_signals FAX SIMILE == PUBLIC_TEST_CHANNEL
+    print('trading-bot',colored('[ACTIVE]','green')+"\nTelegram client connected with: ",colored(USERNAME_TELEGRAM,'red'))
+    
+    # PUBLIC_TEST_CHANNEL FAX SIMILE == freecrypto_signals 
     @client.on(events.NewMessage(chats=PUBLIC_TEST_CHANNEL))
     async def trader_PUBLIC_TEST_CHANNEL(event):
         new_message = event.message.message
@@ -45,37 +46,45 @@ if __name__ == "__main__":
         date = str(event.message.date)
         print('NEW SIGNAL from : ',colored(CHANNEL_1,'green'),'\t', date[:-6],new_message)
 
-        # PARSER
-        op_data={'side':'','symbol':'','buy':{},'sell':{},'stoploss':''}
+        # parser
+        op_data={'side':'','symbol':'','take_profits':[],'entry_prices':[],'stop_losses':[]}
 
-        for row in new_message.split('\n'): # TEXT MESSAGE ROWS 
+        for row in new_message.split('\n'): # text messages
             if '#' in row:
-                op_data['symbol'] = row[1:].replace(' ','')
+                op_data['symbol'] = row[1:].replace(' ','')+'-PERP'
 
             if row.find('Buy') < row.find('Sell'):
-                op_data['side']='Sell'
+                op_data['side']='sell'
             else:
-                op_data['side']='Buy'
+                op_data['side']='buy'
 
             if 'Buy' in row:
                 if '-' in row.split()[1]:
                     temp = row.split()[1].split('-')
                     for n in range(len(temp)):
-                        op_data['buy'][n] = temp[n]
+                        op_data['entry_prices'].append(temp[n])
                 else:
-                    op_data['buy'] = row.split()[1]
+                    op_data['entry_prices'].append(row.split()[1])
             if 'Sell' in row:
                 if '-' in row.split()[1]:
                     temp=row.split()[1].split('-')
                     for n in range(len(temp)):
-                        op_data['sell'][n] = temp[n]
+                        op_data['take_profits'].append(temp[n])
                 else:
-                    op_data['sell'] = row.split()[1]
+                    op_data['take_profits'].append(row.split()[1])
 
             if 'StopLoss' in row or 'Stoploss' in row :
-                op_data['stoploss'] = row.split()[1]
+                if '-' in row.split()[1]:
+                    temp = row.split()[1].split('-')
+                    for n in range(len(temp)):
+                        op_data['stop_losses'].append(temp[n])
+                else:
+                    op_data['stop_losses'].append(row.split()[1])
 
         print(op_data)
     
     with client:
         client.run_until_disconnected()
+
+
+
