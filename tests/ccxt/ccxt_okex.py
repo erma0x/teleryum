2e9e21e5bab443323e9423a8de2e2a62cf7193a0
erma0x <ermano.buikis@gmail.com>
2022-04-13 03:08:19 +0200

    d

--- tests/ccxt/ccxt_okex.py
+++ tests/ccxt/ccxt_okex.py
@@ -5,25 +5,28 @@ import os
 from datetime import datetime
 from dotenv import load_dotenv
 from pprint import pprint
-load_dotenv()
-RRFTID = os.getenv('RRFTID')
-RRFTSEC = os.getenv('RRFTSEC')
 import ccxt
 
-OKEX_READONLY = os.getenv('FTX_READONLY')
-OKEX_READONLY_HASH = os.getenv('FTX_READONLY_HASH')
-OKEX_API_MAIN = os.getenv('FTX_API_MAIN')
-OKEX_API_MAIN_HASH = os.getenv('FTX_API_MAIN_HASH')
+load_dotenv()
+
+OKEX_MAIN_READONLY_ID = os.getenv('OKEX_MAIN_READONLY_ID')
+OKEX_MAIN_READONLY_HASH = os.getenv('OKEX_MAIN_READONLY_HASH')
+OKEX_MAIN_ID = os.getenv('OKEX_MAIN_ID')
+OKEX_MAIN_HASH = os.getenv('OKEX_MAIN_HASH')
 
 exchange = ccxt.okex({
-            'apiKey': OKEX_API_MAIN,
-            'secret': OKEX_API_MAIN_HASH,
+            'apiKey': OKEX_MAIN_ID,
+            'secret': OKEX_MAIN_HASH,
             'enableRateLimit': True,
                 })
+                
+#order = exchange.market(symbol='BTCUSD')
 
+exchange.accounts()
+print( order )
 
-order = exchange.create_limit_buy_order('ETH-PERP', 0.001, 3360.00)
-print(order)
+# order = exchange.create_limit_buy_order('ETH-PERP', 0.001, 2200.00)
+# print(order)
 
 
 # SL_buy = exchange.create_order(symbol='ETH-PERP', type='stop', side='sell', amount=0.001, price=3280.00, params={'triggerPrice':3380.00,'reduceOnly':True })
